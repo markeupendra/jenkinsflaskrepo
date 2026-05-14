9adf884d4ba9dfb844cc82fac83a59ae0e1173b9
markeupendra <markeupendra@gmail.com>
2026-05-14 16:31:46 +0530

    second commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hllo, Jenkins CI/CD from Flask App on Windows!"
+    return "Hello, Jenkins CI/CD from Flask App on Windows!"
 
 
 if __name__=="__main__":
